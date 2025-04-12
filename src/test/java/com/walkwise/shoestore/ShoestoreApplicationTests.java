9aac64c0479143bc8833bc1667ed9f538f13ff09
vaishdeokar <deokarvaishnavi21@gmail.com>
2025-04-12 20:19:16 +0000

    Added Spring Boot project files

---
+++ src/test/java/com/walkwise/shoestore/ShoestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walkwise.shoestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
